60407e62f6ae4983299d325244891fbd98b247ec
sujung-lim <sujunglim94@gmail.com>
2023-06-05 17:17:59 +0900

    style: 탭메뉴 프레임 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./style.css";
 
+// const [showFollowers, setShowFollowers] = useState(false);
+
+// const handleClickFollowers = () => {
+//   setShowFollowers(true);
+// };
+
 function App() {
   return (
     <div className="app">
@@ -48,37 +54,62 @@ function App() {
       <section className="profile screen-container">
         <nav className="top-basic-nav">
           <ul className="top-basic-nav-li">
-            <li><button className="previous-btn"></button></li>
-            <li><button className="more-btn"></button></li>
+            <li>
+              <button className="previous-btn"></button>
+            </li>
+            <li>
+              <button className="more-btn"></button>
+            </li>
           </ul>
         </nav>
         <div className="profile-container">
           <div className="profile-header">
             <div className="followers">
+              {/* onClick 넣을 예정 */}
               <span>2950</span>
               <span>followers</span>
             </div>
-            <img src={require("./img/basic-profile-img.svg").default} alt="프로필 사진" />
+            <img
+              src={require("./img/basic-profile-img.svg").default}
+              alt="프로필 사진"
+            />
             <div className="followings">
               <span>128</span>
               <span>followings</span>
+            </div>
           </div>
-          </div> 
           <div className="profile-main">
             <p>애월읍 위니브 감귤농장</p>
             <p>@ weniv_Mandarin</p>
             <p>애월읍 감귤 전국 배송, 귤따기 체험, 감귤 농장</p>
           </div>
           <div className="profile-footer">
-            <button className="message-btn"></button>
+            <button className="dm-btn"></button>
             <button className="unfollow-btn">언팔로우 </button>
             <button className="share-btn"></button>
           </div>
-          </div>
-        <article>
-
-        </article>
-        <nav className="tab-menu"></nav>
+        </div>
+        <article></article>
+        <nav className="tab-menu">
+          <ul className="tab-menu-list">
+            <div className="tab-menu-container">
+              <li><button className="tab-home-fill-btn"></button></li>
+              <span>홈</span>
+            </div>
+            <div className="tab-menu-container">
+              <li><button className="tab-message-btn"></button></li>
+              <span>채팅</span>
+            </div>
+            <div className="tab-menu-container">
+              <li><button className="tab-message-btn"></button></li>
+              <span>게시물 작성</span>
+            </div>
+            <div className="tab-menu-container">
+              <li><button className="tab-user-btn"></button></li>
+              <span>프로필</span>
+            </div>
+          </ul>
+        </nav>
       </section>
     </div>
   );
